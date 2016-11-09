1bea7c0fb24fd848e9bbc66af15f7ad358765140
tyt2y3 <tyt2y3@gmail.com>
2016-11-09 11:37:53 +0800

    fix conflicts

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-var app, file = 'assets/tab_1_1.mid';
-
-window.onload = function () {
-    app = new VisuApp(20, 3, 'canvas');
-
-    app.run();
-
-    var colors = [[255, 0, 0],
-        [255, 64, 0],
-        [255, 128, 0],
-        [255, 191, 0],
-        [255, 255, 0],
-        [191, 255, 0],
-        [128, 255, 0],
-        [64, 255, 0],
-        [0, 255, 0],
-        [0, 255, 64],
-        [0, 255, 128],
-        [0, 255, 191],
-        [0, 255, 255],
-        [0, 191, 255],
-        [0, 128, 255],
-        [0, 64, 255],
-        [0, 0, 255],
-        [64, 0, 255],
-        [128, 0, 255],
-        [191, 0, 255],
-        [255, 0, 255],
-        [255, 0, 191],
-        [255, 0, 128],
-        [255, 0, 64],
-        [255, 0, 0]];
-
-    MIDI.loadPlugin({
-        soundfontUrl: "https://rawgit.com/gleitz/midi-js-soundfonts/master/FluidR3_GM/",
-=======
 var app, file = 'assets/CanonInD.mid';
 
 window.onload = function () {
@@ -50,7 +13,6 @@ window.onload = function () {
 
     MIDI.loadPlugin({
         soundfontUrl: "https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/FluidR3_GM/",
->>>>>>> ba32cd7... my visualization on Canon in D
         onsuccess: function() {
             console.log('done');
             var player = MIDI.Player;
@@ -60,33 +22,23 @@ window.onload = function () {
                 instruments.push('synth_drum');
 
                 MIDI.loadPlugin({
-<<<<<<< HEAD
-                    soundfontUrl: "https://rawgit.com/gleitz/midi-js-soundfonts/master/FluidR3_GM/",
-=======
                     soundfontUrl: "https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/FluidR3_GM/",
->>>>>>> ba32cd7... my visualization on Canon in D
                     instruments: instruments,
                     onsuccess: function() {
                         var player = MIDI.Player;
 
                         player.loadFile(file, function () {
                             console.log('about to play');
-<<<<<<< HEAD
-
-=======
 /*
->>>>>>> ba32cd7... my visualization on Canon in D
                             MIDI.programChange(0, MIDI.GM.byName['distortion_guitar'].number);
                             MIDI.programChange(2, MIDI.GM.byName['distortion_guitar'].number);
                             MIDI.programChange(4, MIDI.GM.byName['electric_bass_finger'].number);
                             MIDI.programChange(6, MIDI.GM.byName['fx_4_atmosphere'].number);
                             MIDI.programChange(9, 119);
                             MIDI.programChange(10, 119);
-<<<<<<< HEAD
 
                             player.addListener(function(data) {
                                 console.log(data.channel);
-=======
 */
 							var digits = 6;
 							var R = 15;
@@ -105,17 +57,13 @@ window.onload = function () {
                         		}
                             	/*
                                 console.log(data.note, data.channel);
->>>>>>> ba32cd7... my visualization on Canon in D
                                 posTop = 100 + 50 * data.channel + 650 - (data.note * 10);
                                 r = colors[data.channel][0];
                                 g = colors[data.channel][1];
                                 b = colors[data.channel][2];
 
                                 app.addCircle(rgba(r, g, b, 1), 25, posTop);
-<<<<<<< HEAD
-=======
                                 */
->>>>>>> ba32cd7... my visualization on Canon in D
                             });
 
                             player.start();
--- js/visu/canvas.js
+++ js/visu/canvas.js
@@ -1,12 +1,7 @@
 function Canvas(elementId) {
     this.canvas = document.getElementById(elementId);
-<<<<<<< HEAD
-    this.canvas.width = this.width = window.innerHeight * 2;
-    this.canvas.height = this.height = window.innerWidth * 2;
-=======
     this.canvas.width = this.width = window.innerWidth;
     this.canvas.height = this.height = window.innerHeight;
->>>>>>> ba32cd7... my visualization on Canon in D
     this.context = this.canvas.getContext('2d');
     this.shapes = [];
 }
@@ -21,15 +16,11 @@ Canvas.prototype.draw = function () {
 Canvas.prototype.drawCircle = function (circle) {
     this.context.beginPath();
     this.context.arc(circle.centerX, circle.centerY, circle.radius, 0, 2 * Math.PI, false);
-<<<<<<< HEAD
-    this.context.fillStyle = circle.color;
-=======
     var gradient = this.context.createRadialGradient(circle.centerX, circle.centerY, circle.radius/2,
     	circle.centerX, circle.centerY, circle.radius);
 gradient.addColorStop(0, circle.color);
 gradient.addColorStop(1, new RGBA(circle.color.red,circle.color.green,circle.color.blue,0));
     this.context.fillStyle = gradient;
->>>>>>> ba32cd7... my visualization on Canon in D
     this.context.fill();
 };
 Canvas.prototype.empty = function () {
@@ -53,8 +44,4 @@ Canvas.prototype.gc = function () {
 };
 Canvas.prototype.getStartPoint = function (radius) {
     return ((this.width / 2) + (radius * 15));
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> ba32cd7... my visualization on Canon in D
